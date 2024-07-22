c23a9661f81962c78163f86df1bf759dfee87da3
NoobRobloxdev <162366072+NoobRobloxdev@users.noreply.github.com>
2024-07-22 15:31:35 +0200

    Add files via upload

---
+++ NS_console.bat
@@ -0,0 +1,10 @@
+echo Hello to Noob Science Console
+color A
+netstat
+start ncpa.cpl
+color A
+start cmd.exe
+start explorer.exe
+echo start a WW3?
+msg *Hi everyone i will hack you guys HAHHAHAHAHHAHAHAHHAHAHAHA
+
